82783b5cc7e74b47a47ce3e7ae9c4c11aae47d9a
Bartlomiej Kotarski <bkotarski1@gmail.com>
2020-11-03 00:20:14 +0100

    birthday

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Baloon from './assets/baloon.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [isVisible, setVisible] = React.useState(true);
+    const [position, setPosition] = React.useState({x: 0, y: 0});
+
+
+    const handleClick = (event) => {
+        if(isVisible){
+            setVisible(false);
+            return ;
+        }
+        const x = event.nativeEvent.offsetX; // current X
+        const y = event.nativeEvent.offsetY; // current X
+        setVisible(true);
+        setPosition({x, y})
+    }
+
+    React.useEffect(() => {
+        if (!isVisible) return;
+
+        const interval = setInterval(() => {
+            setVisible(false);
+        }, 1500)
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [isVisible, position])
+
+    return (
+        <div className="App" onClick={handleClick}>
+            <div className="background"/>
+            <h1> Wszystkiego Najlepszego
+                <br/>
+                Miśka !!!</h1>
+            <div className="invisible">
+                <img src={Baloon} style={{
+                    display: isVisible ? 'block' : 'none',
+                    top: `${position.y}px`,
+                    left: `${position.x}px`
+                }}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
